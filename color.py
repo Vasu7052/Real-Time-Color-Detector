b49d41092b3291c9d6cc0ae1326c31002cee419c
Vasu Gupta <androfly.official@gmail.com>
2017-10-24 15:05:35 +0530

    Color Sensor

--- color.py
+++ color.py
@@ -51,7 +51,7 @@ while True :
             frame = cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 255), 2)
             cv2.putText(frame, "Yellow Color", (x, y), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 255, 255))
 
-    cv2.imshow('frame' , frame)
+    cv2.imshow('Real Time Color Detector' , frame)
 
     if cv2.waitKey(0) & 0xFF == ord('q') :
         break
